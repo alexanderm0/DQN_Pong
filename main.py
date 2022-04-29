c65c535d77dcfe9102a291db61ad7e1d61d5b976
alexanderm0 <a.mo@student.utwente.nl>
2022-04-29 11:29:14 +0200

    Working model with Python 3.9
    Installed packages, downloaded roms, rewrite imports to updated paths
    
    cant push environment due to size => use 3.9 interpreter (NOT CONDA, just regular python)
    install all packages required using pip install

--- main.py
+++ main.py
@@ -5,7 +5,7 @@ import time
 from collections import deque
 import numpy as np
 
-name = 'PongDeterministic-v4'
+name = 'Pong-v0'
 
 agent = the_agent.Agent(possible_actions=[0,2,3],starting_mem_len=50000,max_mem_len=750000,starting_epsilon = 1, learn_rate = .00025)
 env = environment.make_env(name,agent)
